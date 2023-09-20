4769c60b42dc6157be778dc760eba631f3118dd5
Previtali Nico <nico.previtali@stud.hslu.ch>
2023-09-20 18:48:30 +0200

    Created folder structure, added data + data analysis V1
    Version 1 (without isOrigin, position: versions features)
    - data structure
    - data  cleaning
    - feature engineering
    - data analysis / exploration

---
+++ data_preparation/kialo_data/1_raw_data/load.py
@@ -0,0 +1,46 @@
+import pickle
+import igraph as ig
+
+# load discussions
+dumped_discussions = pickle.load(open("dumped_discussions_first.pickle", "rb"))
+
+# parse_graph
+def parse_discussion_graph(discussion):
+    discussion_graph = ig.Graph()
+
+    vertex_id_mappings = {}
+
+    for i, claim in enumerate(discussion["discussion"]["claims"]):
+        vertex = discussion_graph.add_vertex({
+            "id": claim["id"],
+            "text": claim["text"]
+        })
+        vertex_id_mappings[claim["id"]] = vertex.index
+
+
+    for location in discussion["discussion"]["locations"]:
+
+        if location["isDeleted"] == True:
+            continue
+
+        if location["parentId"] is None:
+            continue
+
+        source = vertex_id_mappings[location["targetId"]]
+        parent = vertex_id_mappings[location["parentId"]]
+
+        discussion_graph.add_edge(
+            source = source, target = parent, relation = location["relation"]
+        )
+
+    ## ignore isolated (no edge) claims
+    discussion_graph.vs.select(_degree=0).delete()
+
+    return discussion_graph
+
+
+parsed_discussion_graphs = [
+    parse_discussion_graph(dumped_discussion)
+    for dumped_discussion in dumped_discussions
+]
+
